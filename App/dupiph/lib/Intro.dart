f2f22e890d759577b9a7e29a9fbf0b1437a37c3f
ksanbal <dev.ksanbal@gmail.com>
2021-03-04 17:56:45 +0900

    인트로 페이지 디자인 부분 적용(애니메이션 적용 후 버튼 삭제 예정)

--- App/dupiph/lib/Intro.dart
+++ App/dupiph/lib/Intro.dart
@@ -1,33 +1,73 @@
 import 'package:flutter/material.dart';
 import 'Index.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
 
 class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TypewriterAnimatedTextKit(
-          text: [
-            "당신의 두피관리앱",
-            "DupiPH",
-          ],
-          textStyle: TextStyle(
-            color: Colors.black,
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
+      body: Stack(
+        alignment: AlignmentDirectional.centerStart,
+        children: [
+          // 이미지 배경
+          Container(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'images/Intro_background.jpeg',
+              fit: BoxFit.fitHeight,
+            ),
           ),
-          speed: Duration(milliseconds: 200),
-          repeatForever: false,
-          totalRepeatCount: 1,
-          onTap: () {
-            print('Tap Event');
-          },
-          onFinished: () {
-            Navigator.pushReplacement(
-                context, MaterialPageRoute(builder: (context) => IndexPage()));
-          },
-        ),
+          // 그라데이션 컬러
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xff259f9b).withOpacity(0.65),
+                  Color(0xff50f1b41).withOpacity(0.65),
+                ],
+              ),
+            ),
+          ),
+          // Text
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  '당신의 두피',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 31,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'DupiPH',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 47,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.arrow_forward,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => IndexPage()),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
