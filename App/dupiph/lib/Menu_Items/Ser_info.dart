1af8fddb6418882c86e7d4a44a2ebc6f4af3f015
ksanbal <dev.ksanbal@gmail.com>
2021-03-04 17:57:19 +0900

    서비스 소개 페이지 작성

---
+++ App/dupiph/lib/Menu_Items/Ser_info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InfoPage_Ser extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('서비스 소개'),
+      ),
+      body: Center(
+        child: Text('서비스 소개입니다.'),
+      ),
+    );
+  }
+}
