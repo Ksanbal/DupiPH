6465501a84131a8f579ced2e80b3a78a3212000e
ksanbal <dev.ksanbal@gmail.com>
2021-03-04 17:57:10 +0900

    회사소개 페이지 작성

---
+++ App/dupiph/lib/Menu_Items/Cop_info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InfoPage_Cop extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('회사소개'),
+      ),
+      body: Center(
+        child: Text('회사 소개입니다.'),
+      ),
+    );
+  }
+}
