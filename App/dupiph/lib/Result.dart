e57bfd7ad3e92072c7425c18cbc1d45e287c7e4a
ksanbal <dev.ksanbal@gmail.com>
2021-03-05 10:17:25 +0900

    결과페이지 인디케이터 레이아웃 적용

--- App/dupiph/lib/Result.dart
+++ App/dupiph/lib/Result.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 import 'Index.dart';
 
@@ -10,19 +12,42 @@ class ResultPage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            flex: 4,
+            flex: 5,
             child: Stack(
               alignment: Alignment.bottomCenter,
               children: [
                 // Background
                 _backGround(),
                 // 측정값 & 인디케이터
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    // 측정결과 Text
+                    _titleText(),
+                    // DateText
+                    SizedBox(height: 5),
+                    _dateText(),
+                    // indicator
+                    SizedBox(height: 20),
+                    _circleIndicator(),
+                    SizedBox(height: 10),
+                  ],
+                ),
               ],
             ),
           ),
           Expanded(
             flex: 5,
-            child: Column(),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // 지성 - 건성 인디케이터
+                  _barIndicator(),
+                ],
+              ),
+            ),
           ),
         ],
       ),
@@ -35,6 +60,14 @@ class ResultPage extends StatelessWidget {
       title: Text('DupiPH'),
       elevation: 0,
       backgroundColor: Color(0xff34a1ae),
+      leading: IconButton(
+          icon: Icon(
+            Icons.refresh,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          }),
       actions: [
         IconButton(
             icon: Icon(
@@ -84,6 +117,124 @@ class ResultPage extends StatelessWidget {
         // Navigator.push(
         //     context, MaterialPageRoute(builder: (context) => 상품페이지()));
       },
+    );
+  }
+
+  _titleText() {
+    return Text(
+      '측정결과',
+      style: TextStyle(
+        fontSize: 17,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  _dateText() {
+    List weekday = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'];
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          '${DateFormat('yyyy.MM.dd').format(DateTime.now())}  ',
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '${weekday[DateTime.now().weekday - 1]}',
+          style: TextStyle(
+            fontSize: 23,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+
+  _circleIndicator() {
+    return Container(
+      width: 200,
+      height: 200,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white,
+      ),
+      child: CircularStepProgressIndicator(
+        totalSteps: 100,
+        currentStep: 74,
+        stepSize: 10,
+        selectedColor: Color(0xff5ee0ef),
+        unselectedColor: Colors.grey[200],
+        padding: 0,
+        width: 150,
+        height: 150,
+        selectedStepSize: 15,
+        roundedCap: (_, __) => true,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '적정',
+              style: TextStyle(
+                fontSize: 40,
+                color: Color(0xff2f7078),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '이대로 유지해주세요.',
+              style: TextStyle(
+                fontSize: 15,
+                color: Color(0xff2f7078),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _barIndicator() {
+    return Column(
+      children: [
+        StepProgressIndicator(
+          totalSteps: 15,
+          currentStep: 12,
+          size: 20,
+          selectedColor: Colors.amber,
+          unselectedColor: Colors.black,
+          roundedEdges: Radius.circular(10),
+          gradientColor: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomRight,
+              colors: [Color(0xffd86565), Color(0xff7464b7)]),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '지성',
+              style: TextStyle(
+                color: Color(0xffd86565),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '건성',
+              style: TextStyle(
+                color: Color(0xff7464b7),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
